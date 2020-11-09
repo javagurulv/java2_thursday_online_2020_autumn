5fa5f049fc7a2224c2edf082a2dbc4365d218296
Aleksandr Grishanov <agr@weissenrieder.com>
2020-11-10 01:27:42 +0200

    thymeleaf simple index page added

--- team_dog/src/main/java/store/controller/MainController.java
+++ team_dog/src/main/java/store/controller/MainController.java
@@ -1,17 +1,12 @@
 package store.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import store.entity.Item;
-import store.entity.types.ItemType;
+import org.springframework.web.bind.annotation.RequestParam;
 import store.service.ItemRetrievalService;
 
-import java.util.Optional;
 
 @Controller
 @RequestMapping("/")
@@ -20,19 +15,14 @@ public class MainController {
     @Autowired
     private ItemRetrievalService itemRetrievalService;
 
-    @Value("${welcome.message:msg_not_found}")
-    private String message;
-
-
     @GetMapping(value = {"/", "/home"})
-    @ResponseBody
-    public String getAll() {
-        itemRetrievalService.saveExampleItem();
-        return message;
+    public String getHomePage() {
+        return "home-page";
     }
 
-    @GetMapping("/item/{type}")
-    public Optional<Item> getItemByType(@PathVariable  ItemType type){
-        return null;
+    @GetMapping("/wares")
+    public String getWarePage(@RequestParam("type") String wareType){
+        System.out.println("ware page test : " + wareType);
+        return "home-page";
     }
 }
