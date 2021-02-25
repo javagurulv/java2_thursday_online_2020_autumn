a8b432a7f29ac5e942e88ff906191c3201325cdc
Maris <maris.baduns@gmail.com>
2021-02-25 15:33:15 +0200

    Lesson_13 - Task_9 - Completed

---
+++ team_marx/src/main/java/book_library/WebBookListApplication.java
@@ -0,0 +1,23 @@
+package book_library;
+
+import book_library.console_ui.ProgramMenu;
+import book_library.web_ui.config.SpringWebConfiguration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication(exclude = HibernateJpaAutoConfiguration.class)
+public class WebBookListApplication {
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(SpringWebConfiguration.class);
+
+        ProgramMenu programMenu = context.getBean(ProgramMenu.class);
+        while (true) {
+            programMenu.print();
+            int menuNumber = programMenu.getMenuNumberFromUser();
+            programMenu.executeSelectedMenuItem(menuNumber);
+        }
+    }
+}
