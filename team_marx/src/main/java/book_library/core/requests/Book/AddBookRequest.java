3882fec39cca2ad265128c34ff62eed6f894512c
Maris <maris.baduns@gmail.com>
2021-02-27 19:04:10 +0200

    Lesson_13 - Task_13 - Completed

--- team_marx/src/main/java/book_library/core/requests/Book/AddBookRequest.java
+++ team_marx/src/main/java/book_library/core/requests/Book/AddBookRequest.java
@@ -21,4 +21,12 @@ public class AddBookRequest {
     public String getAuthor() {
         return author;
     }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
 }
