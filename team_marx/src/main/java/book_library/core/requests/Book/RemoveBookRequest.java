445b6d0646fa8b462e822c10a1e47d0347176f9b
Maris <maris.baduns@gmail.com>
2021-02-27 23:05:18 +0200

    Lesson_13 - added Delete book from list - function

--- team_marx/src/main/java/book_library/core/requests/Book/RemoveBookRequest.java
+++ team_marx/src/main/java/book_library/core/requests/Book/RemoveBookRequest.java
@@ -4,11 +4,18 @@ public class RemoveBookRequest {
 
     private Long bookIdToRemove;
 
+    public RemoveBookRequest() {
+    }
+
     public RemoveBookRequest(Long bookIdToRemove) {
         this.bookIdToRemove = bookIdToRemove;
     }
 
     public Long getBookIdToRemove() {
         return bookIdToRemove;
+    }
+
+    public void setBookIdToRemove(Long bookIdToRemove) {
+        this.bookIdToRemove = bookIdToRemove;
     }
 }
---
+++ team_marx/src/main/java/book_library/web_ui/controllers/DeleteBookController.java
@@ -0,0 +1,37 @@
+package book_library.web_ui.controllers;
+
+import book_library.core.requests.Book.AddBookRequest;
+import book_library.core.requests.Book.RemoveBookRequest;
+import book_library.core.responses.Book.AddBookResponse;
+import book_library.core.responses.Book.RemoveBookResponse;
+import book_library.core.services.Book.RemoveBookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class DeleteBookController {
+
+    @Autowired
+    private RemoveBookService removeBookService;
+
+    @GetMapping(value = "/deleteBookFromList")
+    public String showDeleteBookPage(ModelMap modelMap) {
+        modelMap.addAttribute("request", new RemoveBookRequest());
+        return "deleteBookFromList";
+    }
+
+    @PostMapping("/deleteBookFromList")
+    public String processDeleteBookRequest(@ModelAttribute(value = "request") RemoveBookRequest request, ModelMap modelMap) {
+        RemoveBookResponse response = removeBookService.execute(request);
+        if (response.hasErrors()) {
+            modelMap.addAttribute("errors", response.getErrors());
+            return "deleteBookFromList";
+        } else {
+            return "redirect:/";
+        }
+    }
+}
