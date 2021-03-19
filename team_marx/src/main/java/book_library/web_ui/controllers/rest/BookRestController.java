f30d2be51a4d584c0fa261556a4e5a07097476ac
Maris <maris.baduns@gmail.com>
2021-03-19 06:37:13 +0200

    Lesson_14 - Task_7 - complete

--- team_marx/src/main/java/book_library/web_ui/controllers/rest/BookRestController.java
+++ team_marx/src/main/java/book_library/web_ui/controllers/rest/BookRestController.java
@@ -1,9 +1,24 @@
 package book_library.web_ui.controllers.rest;
 
+import book_library.core.requests.Book.GetBookRequest;
+import book_library.core.responses.Book.GetBookResponse;
+import book_library.core.services.Book.GetBookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/book")
 public class BookRestController {
+
+    @Autowired private GetBookService getBookService;
+
+    @GetMapping(path = "/{id}", produces = "application/json")
+    public GetBookResponse getBook(@PathVariable Long id) {
+        GetBookRequest request = new GetBookRequest(id);
+        return getBookService.execute(request);
+    }
+
 }
