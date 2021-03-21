e4bff3c618eb11b8a709e3dad498c651eb448e4c
Maris <maris.baduns@gmail.com>
2021-03-21 13:37:18 +0200

    Lesson_14 - Task_11 - Complete

--- team_marx/src/main/java/book_library/web_ui/controllers/rest/BookRestController.java
+++ team_marx/src/main/java/book_library/web_ui/controllers/rest/BookRestController.java
@@ -2,10 +2,13 @@ package book_library.web_ui.controllers.rest;
 
 import book_library.core.requests.Book.AddBookRequest;
 import book_library.core.requests.Book.GetBookRequest;
+import book_library.core.requests.Book.UpdateBookRequest;
 import book_library.core.responses.Book.AddBookResponse;
 import book_library.core.responses.Book.GetBookResponse;
+import book_library.core.responses.Book.UpdateBookResponse;
 import book_library.core.services.Book.AddBookService;
 import book_library.core.services.Book.GetBookService;
+import book_library.core.services.Book.UpdateBookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,6 +18,7 @@ public class BookRestController {
 
     @Autowired private GetBookService getBookService;
     @Autowired private AddBookService addBookService;
+    @Autowired private UpdateBookService updateBookService;
 
     @GetMapping(path = "/{id}", produces = "application/json")
     public GetBookResponse getBook(@PathVariable Long id) {
@@ -29,4 +33,10 @@ public class BookRestController {
         return addBookService.execute(request);
     }
 
+    @PutMapping(path = "/{id}",
+            consumes = "application/json",
+            produces = "application/json")
+    public UpdateBookResponse updateBook (@RequestBody UpdateBookRequest request) {
+        return updateBookService.execute(request);
+    }
 }
