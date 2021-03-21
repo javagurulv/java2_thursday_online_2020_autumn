40c28f57300612cb1685d58e172d609cd949dd89
Maris <maris.baduns@gmail.com>
2021-03-21 13:27:53 +0200

    Lesson_14 - Task_11 - Added UpdateBookRequestValidator

--- team_marx/src/main/java/book_library/core/validators/Book/UpdateBookRequestValidator.java
+++ team_marx/src/main/java/book_library/core/validators/Book/UpdateBookRequestValidator.java
@@ -1,8 +1,32 @@
 package book_library.core.validators.Book;
 
+import book_library.core.requests.Book.UpdateBookRequest;
+import book_library.core.responses.CoreError;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Component
 public class UpdateBookRequestValidator {
 
+    public List<CoreError> validate(UpdateBookRequest request) {
+        List<CoreError> errors = new ArrayList<>();
+        validateTitle(request).ifPresent(errors::add);
+        validateAuthor(request).ifPresent(errors::add);
+        return errors;
+    }
+
+    private Optional<CoreError> validateTitle(UpdateBookRequest request) {
+        return (request.getNewTitle() == null || request.getNewTitle().isEmpty())
+                ? Optional.of(new CoreError("newTitle", "Must not be empty!"))
+                : Optional.empty();
+    }
+
+    private Optional<CoreError> validateAuthor(UpdateBookRequest request) {
+        return (request.getNewAuthor() == null || request.getNewAuthor().isEmpty())
+                ? Optional.of(new CoreError("newAuthor", "Must not be empty!"))
+                : Optional.empty();
+    }
 }
