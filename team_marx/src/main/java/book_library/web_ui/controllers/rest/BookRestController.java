ee0f035b4962ce80eaf133cf1c29ee2348c57c00
Maris <maris.baduns@gmail.com>
2021-03-21 14:34:08 +0200

    Lesson_14 - Task_13 - Complete

--- team_marx/src/main/java/book_library/web_ui/controllers/rest/BookRestController.java
+++ team_marx/src/main/java/book_library/web_ui/controllers/rest/BookRestController.java
@@ -1,12 +1,15 @@
 package book_library.web_ui.controllers.rest;
 
 import book_library.core.requests.Book.AddBookRequest;
+import book_library.core.requests.Book.DeleteBookRequest;
 import book_library.core.requests.Book.GetBookRequest;
 import book_library.core.requests.Book.UpdateBookRequest;
 import book_library.core.responses.Book.AddBookResponse;
+import book_library.core.responses.Book.DeleteBookResponse;
 import book_library.core.responses.Book.GetBookResponse;
 import book_library.core.responses.Book.UpdateBookResponse;
 import book_library.core.services.Book.AddBookService;
+import book_library.core.services.Book.DeleteBookService;
 import book_library.core.services.Book.GetBookService;
 import book_library.core.services.Book.UpdateBookService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +22,7 @@ public class BookRestController {
     @Autowired private GetBookService getBookService;
     @Autowired private AddBookService addBookService;
     @Autowired private UpdateBookService updateBookService;
+    @Autowired private DeleteBookService deleteBookService;
 
     @GetMapping(path = "/{id}", produces = "application/json")
     public GetBookResponse getBook(@PathVariable Long id) {
@@ -39,4 +43,12 @@ public class BookRestController {
     public UpdateBookResponse updateBook (@RequestBody UpdateBookRequest request) {
         return updateBookService.execute(request);
     }
+
+    @DeleteMapping (path = "/{id}", produces = "application/json")
+    public DeleteBookResponse deleteBook (@PathVariable Long id) {
+        DeleteBookRequest request = new DeleteBookRequest(id);
+        return deleteBookService.execute(request);
+    }
+
+
 }
